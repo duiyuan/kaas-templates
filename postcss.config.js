e854a550f09aee9594a9f0dce2e5d3529a194353
rick <1196363729@qq.com>
2023-02-06 11:10:26 +0800

    feat: change postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,38 +1,40 @@
 const { useTailwind } = require('./scripts/constant')
 
+const postcssPlugins = !useTailwind
+  ? [
+      'postcss-flexbugs-fixes',
+      [
+        'postcss-preset-env',
+        {
+          autoprefixer: {
+            flexbox: 'no-2009',
+          },
+          stage: 3,
+        },
+      ],
+      // Adds PostCSS Normalize as the reset css with default options,
+      // so that it honors browserslist config in package.json
+      // which in turn let's users customize the target behavior as per their needs.
+      'postcss-normalize',
+    ]
+  : [
+      'tailwindcss',
+      'postcss-flexbugs-fixes',
+      [
+        'postcss-preset-env',
+        {
+          autoprefixer: {
+            flexbox: 'no-2009',
+          },
+          stage: 3,
+        },
+      ],
+    ]
+
 module.exports = {
   // Necessary for external CSS imports to work
   // https://github.com/facebook/create-react-app/issues/2677
   ident: 'postcss',
   config: false,
-  plugins: !useTailwind
-    ? [
-        'postcss-flexbugs-fixes',
-        [
-          'postcss-preset-env',
-          {
-            autoprefixer: {
-              flexbox: 'no-2009',
-            },
-            stage: 3,
-          },
-        ],
-        // Adds PostCSS Normalize as the reset css with default options,
-        // so that it honors browserslist config in package.json
-        // which in turn let's users customize the target behavior as per their needs.
-        'postcss-normalize',
-      ]
-    : [
-        'tailwindcss',
-        'postcss-flexbugs-fixes',
-        [
-          'postcss-preset-env',
-          {
-            autoprefixer: {
-              flexbox: 'no-2009',
-            },
-            stage: 3,
-          },
-        ],
-      ],
+  plugins: [...postcssPlugins],
 }
