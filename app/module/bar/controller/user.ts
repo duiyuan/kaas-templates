5670f01d3bf5e568388bb7d940bcb02c105872e4
rick <1196363729@qq.com>
2023-02-16 17:10:58 +0800

    feat: lint

--- app/module/bar/controller/user.ts
+++ app/module/bar/controller/user.ts
@@ -1,18 +1,18 @@
-import { Inject, HTTPController, HTTPMethod, HTTPMethodEnum, HTTPQuery } from '@eggjs/tegg';
-import { HelloService } from '@/module/foo';
+import { Inject, HTTPController, HTTPMethod, HTTPMethodEnum, HTTPQuery } from '@eggjs/tegg'
+import { HelloService } from '@/module/foo'
 
 @HTTPController({
   path: '/bar',
 })
 export class UserController {
   @Inject()
-  helloService: HelloService;
+  helloService: HelloService
 
   @HTTPMethod({
     method: HTTPMethodEnum.GET,
     path: 'user',
   })
   async user(@HTTPQuery({ name: 'userId' }) userId: string) {
-    return await this.helloService.hello(userId);
+    return await this.helloService.hello(userId)
   }
 }
