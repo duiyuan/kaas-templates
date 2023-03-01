a50df94dc23de048211992f739bac59cf0a1053e
rick <1196363729@qq.com>
2023-03-01 15:26:47 +0800

    chore: update husky

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 import '@testing-library/jest-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+test('renders Hello link', () => {
+  render(<App />, { wrapper: BrowserRouter })
+  const linkElement = screen.getByText(/Hello/i)
   expect(linkElement).toBeInTheDocument()
 })
